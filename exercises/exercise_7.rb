94ddc005b2dc0092fc12857fdbfe766186666645
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-17 20:38:16 -0400

    Fulfilled basic requirements

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -11,3 +11,10 @@ puts "----------"
 
 # Your code goes here ...
 
+class Employee
+  validates: first_name, presence: true
+  validates: last_name, presence: true
+  validates: hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+end
+
+
